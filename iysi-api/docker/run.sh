f543d469cd0258421f4bd05e15d5ab5e8ec1b709
Nnamani John <nnamanijohn123@gmail.com>
2024-06-28 11:01:05 +0100

    docker config files

---
+++ iysi-api/docker/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd /var/www
+
+#php artisan migrate:fresh --seed --force
+php artisan migrate
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan storage:link
+
+/usr/bin/supervisord -c /etc/supervisord.conf
